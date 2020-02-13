f4abcb33a51669524c00c55c18fa6a442e97d4dd
Jonathan Tafel <tafelito@gmail.com>
2020-02-13 13:34:57 -0500

    handle out-of-order responses

--- src/hooks/use-fetch-content.tsx
+++ src/hooks/use-fetch-content.tsx
@@ -31,17 +31,10 @@ export function useFetchContent({
 
   const [state, setState] = useReducer(reducer, initialState);
 
-  let isLoading = state.loading;
-
-  //   handle stale data in case of props change
-  if (type !== prevType) {
-    // Type changed since last render. Update state.
-    setState(initialState);
-    setPrevType(type);
-    isLoading = true;
-  }
+  // let isLoading = state.loading;
 
   useEffect(() => {
+    let ignore = false;
     // pixabay API url (if type = video it will search for video content)
     const api_url = `${process.env.REACT_APP_PIXABAY_API_URL}${type}`;
     // using they key from .env doesn't hide the key from the source
@@ -52,32 +45,51 @@ export function useFetchContent({
       .then(async response => {
         if (response.status === 200) {
           const res = await response.json();
-          setState({
-            data: res,
-            loading: false,
-          });
+          if (!ignore) {
+            setState({
+              data: res,
+              loading: false,
+            });
+          }
         } else {
           // The Promise returned from fetch() won't reject on HTTP error status even if the response is an HTTP 404 or 500.
           // Instead, it will resolve normally, and it will only reject on network failure or if anything prevented the request from completing.
           // https://github.com/github/fetch#caveats
-          setState({
-            data: undefined,
-            error: `Error fetching content: ${response.statusText}`,
-            loading: false,
-          });
+          if (!ignore) {
+            setState({
+              data: undefined,
+              error: `Error fetching content: ${response.statusText}`,
+              loading: false,
+            });
+          }
         }
       })
       .catch(error => {
-        setState({
-          data: undefined,
-          error: `Error fetching content: ${error.message}`,
-          loading: false,
-        });
+        if (!ignore) {
+          setState({
+            data: undefined,
+            error: `Error fetching content: ${error.message}`,
+            loading: false,
+          });
+        }
       });
+    return () => {
+      ignore = true;
+    };
   }, [type, q, page, perPage]);
 
-  return {
-    ...state,
-    loading: isLoading,
-  };
+  //   handle stale data in case of props change
+  if (type !== prevType) {
+    // Type changed since last render. Update state.
+    setState(initialState);
+    setPrevType(type);
+    // isLoading = true;
+    return initialState;
+  }
+
+  // return {
+  //   ...state,
+  //   loading: isLoading,
+  // };
+  return state;
 }
